6df4d79483e1d50eed7fed1a0f1fab1942f8bdbb
Heather King <hkingdevelopment@gmail.com>
2021-10-12 20:19:47 +0100

    adding homework exercises b c and d

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -55,13 +55,49 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+print(users["Jonathan"]["twitter"])
 
 # 2. Get Erik's hometown
+print(users["Erik"]["home_town"])
+
 # 3. Get the array of Erik's lottery numbers
+print(users["Erik"]["lottery_numbers"])
+
 # 4. Get the species of Avril's pet Monty
+print(users["Avril"]["pets"][0]["species"])
+
 # 5. Get the smallest of Erik's lottery numbers
+print(min(users["Erik"]["lottery_numbers"]))
+
+# OR using a loop
+lottery_numbers = users["Erik"]["lottery_numbers"]
+minimum = lottery_numbers[0]
+
+for number in lottery_numbers:
+    if minimum > number:
+      minimum = number
+print(minimum)
+
 # 6. Return an array of Avril's lottery numbers that are even
+avril_numbers = users["Avril"]["lottery_numbers"]
+
+for number in avril_numbers:
+  if number % 2 == 0:
+    print(number, end = " ")
+
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+(users["Erik"]["lottery_numbers"]).append(7)
+print(users["Erik"]["lottery_numbers"])
+
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"]["home_town"] = "Edinburgh"
+print(users["Erik"]["home_town"])
+
 # 9. Add a pet dog to Erik called "Fluffy"
+(users["Erik"]["pets"]).append({"name" : "Fluffy", "species": "dog"})
+print((users["Erik"]["pets"]))
+
 # 10. Add another person to the users dictionary
+users["Bob"] = {}
+print(users)
--- exercise_d_advanced_logic.py
+++ exercise_d_advanced_logic.py
@@ -3,18 +3,52 @@
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # 1. Print out a list of the even integers:
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
 
+for number in numbers:
+    if number % 2 == 0:
+        print(number, end = " ")
 
 # 2. Print the difference between the largest and smallest value:
+maximum = max(numbers)
+minimum = min(numbers)
+
+print(maximum - minimum)
 
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
+previous_number = 0
+
+for number in numbers:
+    if number == 2 and previous_number == 2:
+        print(True)
+    previous_number = number
 
 
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
-#    
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
+#
+#    numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
+#    so we want to add 1, 1
+
+sum = 0
+index = 0
+
+index_of_6 = numbers.index(6)
+print(index_of_6)
+index_of_7 = numbers.index(7)
+print(index_of_7)
+
+for number in numbers:
+    sum += number
+
+# don't know (yet)!! the next exercise was easier than this one!... will have to come back to it. 
+    
+print(sum)
+
 
 
 # 5. HARD! Print the sum of the numbers. 
@@ -23,6 +57,23 @@ numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 #    HINT - You will need to track the index throughout the loop.
 #
 #    So [5, 13, 2] would have sum of 5. 
+
+#    numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
+
+index = 0
+sum = 0
+
+while index < len(numbers):
+    number = numbers[index]
+    if number == 13:
+        pass
+        index += 2
+    else:
+        sum += number
+        index += + 1
+
+print(sum)
+
 
 
 
